9986c2f1a5bed3e5f902b5d0093df345cf8b4a23
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-08-27 16:04:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function highlight() {
     //Write your code here
 
+	   const boldElements = document.querySelectorAll('strong');
+    
+    // Iterate through all <strong> elements and change their color to green
+    boldElements.forEach(element => {
+        element.style.color = 'green';
+    });
+
 
 }
 
 
 function return_normal() {
     //Write your code here
+
+	  const boldElements = document.querySelectorAll('strong');
+    
+    // Iterate through all <strong> elements and change their color back to black
+    boldElements.forEach(element => {
+        element.style.color = 'black';
+    });
 
     
 }
